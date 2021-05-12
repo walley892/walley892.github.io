4f54d8f3ac19642efef2a51be250947c3f20c0fd
Evan <evanwall@buffalo.edu>
2021-05-12 16:00:12 -0700

    add canvas background

--- canvasController.js
+++ canvasController.js
@@ -1,7 +1,32 @@
+import {placeNodes, getNodes} from "oscController";
+var canvasWidth, canvasHeight;
+var canvas;
 function setUpCanvas() {
-	var canvas = document.getElementById("mainCanvas");
+	canvas = document.getElementById("mainCanvas");
 	canvas.width = document.body.clientWidth;
 	canvas.height = document.body.clientWidth;
+	canvasWidth = canvas.width;
+	canvasHeight = canvas.height;
+}
+
+function drawOscNode(node){
+	var ctx = canvas.getContext("2d");
+	ctx.beginPath();
+	ctx.arc(node.posX, node.posY, 25, 2*Math.PI);
+	ctx.fillStyle = 'rgb(255, 0, 0)';
+	ctx.fill();
+}
+
+function initOscContoller(){
+	placeNodes(25, 1, canvasHeight/2, canvasWidth/2);
+}
+
+function drawOscNodes(){
+	var nodes = getNodes();
+	for(var i = 0; i < nodes.length; ++i){
+		drawOscNode(nodes[i]);
+	}
 }
 
 setUpCanvas();
+placeNodes();
---
+++ oscController.js
@@ -0,0 +1,34 @@
+class Node{
+	constructor(posX, posY){
+            this.posX = posX;
+            this.posY = posY;
+            this.state = Math.random();
+            this.neighbors = [];
+        }
+}
+
+var nodes = [];
+
+function polarToCartesian(r, theta){
+	return [r*Math.cos(theta), r*Math.sin(theta)];
+}
+
+//place nodes of radius radius in n_rings concentric rings
+function placeNodes(radius, n_rings, centerX, centerY){
+	// Add the center node
+	nodes.push(new Node(centerX, centerY));
+	for (var i = 0; i < n_rings; ++i){
+		var bigRadius = 2*radius*(i+1);
+		nodes_in_ring = Math.PI/(Math.asin(radius/bigRadius));
+		var deltaAngle = Math.PI/nodes_in_ring;
+		for(var j = 0; j < nodes_in_ring; ++j){
+			var angle = deltaAngle*j;
+			var pos = polarToCartesian(bigRadius, angle);
+			nodes.push(new Node(centerX + pos[0], centerY + pos[1]));
+		}
+	}
+}
+
+function getNodes(){
+	return nodes;
+}
