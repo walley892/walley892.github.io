3311cb863988b20b64f508f180ac76cf2213803d
Evan <evanwall@buffalo.edu>
2021-05-12 16:21:57 -0700

    fix import

--- canvasController.js
+++ canvasController.js
@@ -1,4 +1,4 @@
-import {placeNodes, getNodes} from "./oscController";
+import {placeNodes, getNodes} from "./oscController.js";
 var canvasWidth, canvasHeight;
 var canvas;
 function setUpCanvas() {
