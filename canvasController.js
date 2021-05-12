39b6e67ecdeb5cbec6f0dd69dc3ec8c72ac59ce9
Evan <evanwall@buffalo.edu>
2021-05-12 14:57:37 -0700

    set up canvas

---
+++ canvasController.js
@@ -0,0 +1,7 @@
+function setUpCanvas() {
+	var canvas = document.getElementById("mainCanvas");
+	canvas.width = document.body.clientWidth;
+	canvas.height = document.body.clientWidth;
+}
+
+setUpCanvas();
