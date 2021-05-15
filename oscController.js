a6185842d627e4a1a5111e78fac75224ee08f2f8
Evan <evanwall@buffalo.edu>
2021-05-14 17:05:37 -0700

    fix node sizing

--- oscController.js
+++ oscController.js
@@ -18,12 +18,12 @@ function polarToCartesian(r, theta){
 //place n_nodes[i] rings around the ith concentric ring of radius radii[i]
 function placeNodes(n_nodes, radii, centerX, centerY){
 	// Add the center node
-	nodes.push(new Node(centerX, centerY));
+	nodes.push(new Node(centerX, centerY, 50));
 	for (var i = 0; i < radii.length; ++i){
 		var nodes_in_ring = n_nodes[i];
 		var bigradius = radii[i];
 		var deltaAngle = (2*Math.PI)/nodes_in_ring;
-		var littleradius = bigradius*Math.sin(Math.PI/bigradius);
+		var littleradius = bigradius*Math.sin(Math.PI/nodes_in_ring);
 		for(var j = 0; j < nodes_in_ring; ++j){
 			var angle = deltaAngle*j;
 			var pos = polarToCartesian(bigradius, angle);
