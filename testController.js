6c55e2f542554c20a9c07f751a7cd936b2e31b30
Evan <evanwall@buffalo.edu>
2021-05-17 00:21:11 -0700

    add basic fragment program

--- testController.js
+++ testController.js
@@ -4,4 +4,8 @@ canvas.height = window.innerHeight;
 
 
 var glslCanvas = new GlslCanvas(canvas);
+
+var frag = "main() {\ngl_FragColor = vec4(1.0, 0, 0, 1);\n}\n";
+
+glslCanvas.load(frag);
 export {canvas, glslCanvas};
