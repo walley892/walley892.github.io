c04420848e38e9887ea4dd61113f3be4cd43c2f0
Evan <evanwall@buffalo.edu>
2021-05-17 00:10:57 -0700

    make canvas fullscreen, export canvas for debugging

--- testController.js
+++ testController.js
@@ -1,2 +1,7 @@
 var canvas = document.getElementById("mainCanvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
 var glslCanvas = new GlslCanvas(canvas);
+export {canvas, glslCanvas};
