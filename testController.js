008395276a20d422dba6bb152b36e5e7b0630f1f
Evan <evanwall@buffalo.edu>
2021-05-17 00:30:25 -0700

    test simple gradient

--- testController.js
+++ testController.js
@@ -5,7 +5,7 @@ canvas.height = window.innerHeight;
 
 var glslCanvas = new GlslCanvas(canvas);
 
-var frag = "main() {\ngl_FragColor = vec4(1.0, 0, 0, 1);\n}\n";
+var frag = "uniform mediump vec2 u_resolution;\n void main() {\ngl_FragColor = vec4(gl_FragCoord.x/u_resolution.x, 0, 0, 1);\n}\n";
 
 glslCanvas.load(frag);
 export {canvas, glslCanvas};
