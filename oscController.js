c0e877e43f4b3f73a0a844fc0bbfe41fb99f2b6e
Evan <evanwall@buffalo.edu>
2021-05-20 00:57:16 -0700

    convert main page to webgl

--- oscController.js
+++ oscController.js
@@ -147,7 +147,7 @@ class OscSceneController extends SceneController{
 	}
 	drawScene(){
 		this.glslCanvas.setUniform("u_position", ...(nodes.map((node) => [node.posX, node.posY])));
-		this.glslCanvas.setUniform("u_state", ...(nodes.map((node) => node.state)));
+		this.glslCanvas.setUniform("u_state", ...(nodes.map((node) => node.state).slice(0, 5)));
 	}
 	update(){
 		updateNodes();
