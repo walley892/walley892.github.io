965e1bcbe4ca9d440f756be9e5b114ec1cd33710
Evan <evanwall@buffalo.edu>
2021-05-20 03:27:09 -0700

    convert main page to webgl

--- oscController.js
+++ oscController.js
@@ -60,7 +60,7 @@ function getNodes(){
 
 function updateState(){
 	for(var i = 0; i < nodes.length; ++i){
-		nodes[i].state = nodes[i].state + 0.001;
+		nodes[i].state = nodes[i].state + 0.01;
 		nodes[i].prevState = nodes[i].state;
 	}
 }
@@ -69,7 +69,7 @@ function kickNeighbors(){
 	for(var i = 0; i < nodes.length; ++i){
 		if(nodes[i].prevState >=1){
 			for(var j = 0; j < nodes[i].neighbors.length; ++j){
-				nodes[i].neighbors[j].state += 0.005;
+				nodes[i].neighbors[j].state += 0.02;
 			}
 		}
 	}
