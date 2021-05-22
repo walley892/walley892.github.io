3b17a5f14ed8bdc4f993358197262c2824d2e64f
Evan <evanwall@buffalo.edu>
2021-05-21 23:46:10 -0700

    testing domain coloring

--- testController.js
+++ testController.js
@@ -1,25 +1,27 @@
 import {TestSceneController} from "./testCon.js";
+import {loadText} from "./utils.js";
 
 class SiteController{
 	constructor(){
 		this.canvas = document.getElementById("mainCanvas");
 		this.glslCanvas = new GlslCanvas(this.canvas);
-		this.canvas.width = window.innerWidth;
-		this.canvas.height = window.innerHeight;
+		this.canvas.width = document.documentElement.clientWidth;
+		this.canvas.height = document.documentElement.clientHeight;
 		this.activeScene = null;
 	}
 	setScene(sceneControllerCls){
-		this.activeScene = new sceneControllerCls(this.canvas);
+		this.activeScene = new sceneControllerCls(this.canvas, this.glslCanvas);
+		loadText(this.activeScene.fragFile()).then((frag) => this.glslCanvas.load(frag));
 	}
 	startActiveScene(){
 		this.activeScene.initScene();
 		this.activeSceneUpdate = window.setInterval(
 			this.activeScene.update.bind(this.activeScene),
-			10
+			20
 		);
 		this.activeSceneDraw = window.setInterval(
 			this.activeScene.drawScene.bind(this.activeScene),
-			10
+			20
 		);
 	}
 }
@@ -27,4 +29,5 @@ class SiteController{
 var siteController = new SiteController();
 
 siteController.setScene(TestSceneController);
-//siteController.startActiveScene();
+siteController.startActiveScene();
+export {siteController};
