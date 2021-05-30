4846d0c91148380566a651ff534ed1545961a0cc
Evan <evanwall@buffalo.edu>
2021-05-30 01:00:19 -0700

    add second test page

---
+++ testCon1.js
@@ -0,0 +1,24 @@
+import {SceneController} from "./base_scene_controller.js";
+import {baseScreenWidth, baseScreenHeight} from "./magicNumbers.js";
+
+class TestSceneController extends SceneController{
+	constructor(canvas, glslCanvas){
+		super(canvas, glslCanvas);
+		this.glslCanvas = glslCanvas;
+		this.T = 0;
+
+	}
+	initScene(){
+	}
+	fragFile(){
+		return "testFrag.frag";
+	}
+	drawScene(){
+		this.glslCanvas.setUniform("u_t", this.T/80);
+	}
+	update(){
+		this.T = this.T + 1;
+	}
+}
+
+export {TestSceneController};
---
+++ testController1.js
@@ -0,0 +1,33 @@
+import {TestSceneController} from "./testCon.js";
+import {loadText} from "./utils.js";
+
+class SiteController{
+	constructor(){
+		this.canvas = document.getElementById("mainCanvas");
+		this.glslCanvas = new GlslCanvas(this.canvas);
+		this.canvas.width = document.documentElement.clientWidth;
+		this.canvas.height = document.documentElement.clientHeight;
+		this.activeScene = null;
+	}
+	setScene(sceneControllerCls){
+		this.activeScene = new sceneControllerCls(this.canvas, this.glslCanvas);
+		loadText(this.activeScene.fragFile()).then((frag) => this.glslCanvas.load(frag));
+	}
+	startActiveScene(){
+		this.activeScene.initScene();
+		this.activeSceneUpdate = window.setInterval(
+			this.activeScene.update.bind(this.activeScene),
+			20
+		);
+		this.activeSceneDraw = window.setInterval(
+			this.activeScene.drawScene.bind(this.activeScene),
+			20
+		);
+	}
+}
+
+var siteController = new SiteController();
+
+siteController.setScene(TestSceneController);
+siteController.startActiveScene();
+export {siteController};
