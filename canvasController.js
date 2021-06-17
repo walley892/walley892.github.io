da37f72a8d01d11c8e770b4da3b35fa92bf9a453
Evan <evanwall@buffalo.edu>
2021-06-16 22:55:31 -0700

    apply sizing changes to main page

--- canvasController.js
+++ canvasController.js
@@ -5,8 +5,6 @@ class SiteController{
 	constructor(){
 		this.canvas = document.getElementById("mainCanvas");
 		this.glslCanvas = new GlslCanvas(this.canvas);
-		this.canvas.width = document.documentElement.clientWidth;
-		this.canvas.height = document.documentElement.clientHeight;
 		this.activeScene = null;
 	}
 	setScene(sceneControllerCls){
