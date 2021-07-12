e92eff7862bdd15f7ce1bf81f1ba1f92091e778d
Evan Walley <evanwall@buffalo.edu>
2021-07-12 13:26:57 -0700

    fix for initial canvas sizing on macos

--- eyeController.js
+++ eyeController.js
@@ -1,10 +1,23 @@
 import {SceneController} from "./base_scene_controller.js";
 import {baseScreenWidth, baseScreenHeight} from "./magicNumbers.js";
 
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: (evt.clientX - rect.left) / (rect.right - rect.left) * canvas.width,
+        y: (evt.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height
+    };
+}
+
 class EyeSceneController extends SceneController{
 	constructor(canvas, glslCanvas){
 		super(canvas, glslCanvas);
+		this.canvas = canvas;
 		this.glslCanvas = glslCanvas;
+		this.mouse = {x:0, y:0};
+		document.addEventListener('mouseMove', function(e) {
+			this.mouse = getMousePos(this.canvas, e);
+		}.bind(this));
 		this.T = 0;
 
 	}
@@ -23,7 +36,7 @@ class EyeSceneController extends SceneController{
 	}
 	drawScene(){
 		this.glslCanvas.setUniform("u_t", this.T/80);
-		this.glslCanvas.setUniform("mouse_pos", 0, 0);
+		this.glslCanvas.setUniform("mouse_pos", this.mouse.x, this.mouse.y);
 	}
 	update(){
 		this.T = this.T + 1;
