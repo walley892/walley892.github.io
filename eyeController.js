e7c624d2905bf4b39a55432d9bcf3b98675bd986
Evan Walley <evanwall@buffalo.edu>
2021-07-14 09:24:03 -0700

    place images on projects page

--- eyeController.js
+++ eyeController.js
@@ -8,12 +8,24 @@ class EyeSceneController extends SceneController{
 		this.canvas = canvas;
 		this.glslCanvas = glslCanvas;
 		this.T = 0;
-
+		this._elements = [];
 	}
 	createHtml(){
-		var img = document.createElement("img");
-		img.src = "./earth.jpg";
-		document.body.appendChild(img);
+		var elementWidth = this.canvas.width/5.0;
+		var elementHeight = this.canvas.height/4.0;
+		for(var j = 0; j < 3; ++j){
+			for(var i = 0; i < 2; ++i){
+				var img = document.createElement("img");
+				img.src = "./earth.jpg";
+				img.style.width = elementWidth + "px";
+				img.style.height = elementHeight + "px";
+				img.style.position = "absolute";
+				img.style.top = (i+1)*elementHeight/3 - elementHeight/2;
+				img.style.left = (j+i)*elementWidth/4 - elementWidth/2;
+				document.body.appendChild(img);
+				this._elements.push(img);
+			}
+		}
 	}
 	placeHtml(){
 
